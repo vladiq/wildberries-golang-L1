85a3f6b0180b28396879a8bb5a4f82c42054f064
vladiq <vladislav.kosogorov21@gmail.com>
2022-08-29 18:00:21 +0300

    fix typo

--- tasks/18/main.go
+++ tasks/18/main.go
@@ -10,23 +10,23 @@ type safeCounter struct {
 	counter int
 }
 
-func (sm *safeCounter) increment(wg *sync.WaitGroup) {
+func (sc *safeCounter) increment(wg *sync.WaitGroup) {
 	defer wg.Done()
 
-	sm.mu.Lock()
-	defer sm.mu.Unlock()
-	(*sm).counter++
+	sc.mu.Lock()
+	defer sc.mu.Unlock()
+	(*sc).counter++
 }
 
 func main() {
-	sm := safeCounter{}
+	sc := safeCounter{}
 
 	var wg sync.WaitGroup
 	for i := 0; i < 1e6; i++ {
 		wg.Add(1)
-		go sm.increment(&wg)
+		go sc.increment(&wg)
 	}
 
 	wg.Wait()
-	fmt.Println(sm.counter)
+	fmt.Println(sc.counter)
 }
